87714f56fdc585b097857a3909faab3acb9c4dd0
Evgeniy Goncharuck <iterlace.dev@sent.com>
2023-05-18 18:06:01 +0300

    task4 completed

--- task_4.py
+++ task_4.py
@@ -1,10 +1,23 @@
 import re
 
-separators = re.compile(r";:\?")
-pattern = re.compile(r"", re.I)
+separators = re.compile(r"")
+pattern = re.compile(
+    r"(\d{1,15})[;:?]"
+    r"([a-z\-']{1,25})[;:?]"
+    r"([a-z\d\-'\s]{1,20})[;:?]"
+    r"(L|p|S|lab)[;:?]"
+    r"([a-z\-']{1,29})[;:?]"
+    r"([a-z\-']{1,30})[;:?]"
+    r"(\d{1,5})[;:?]"
+    r"(\d{1,4})[;:?]"
+    r"(\d{1,4})[;:?]"
+    r"([a-z\d]{1,6})[;:?]"
+    r"(\d+)",
+    re.I,
+)
 
 
-def validate(line: str) -> (bool, str):
+def validate(line: str) -> bool:
     match = pattern.match(line)
     if match is None:
         return False
@@ -15,11 +28,11 @@ def validate(line: str) -> (bool, str):
 def main() -> None:
     with open("input_4.csv", "r") as f:
         for line in f.readlines():
-            is_valid, comment = validate(line)
+            is_valid = validate(line)
             if is_valid:
-                print(f"Line is valid: {line}")
+                print(f"Line is valid: {line.strip()}")
             else:
-                print(f"Line is invalid ({comment}): {line}")
+                print(f"Line is invalid: {line.strip()}")
 
 
 if __name__ == "__main__":
