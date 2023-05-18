6c672d7fc000f9179c087c342190cbcb1e1316b4
Evgeniy Goncharuck <iterlace.dev@sent.com>
2023-05-18 18:13:29 +0300

    task 5

---
+++ task_5.py
@@ -0,0 +1,14 @@
+from task_4 import pattern  # isort: skip
+
+
+def main() -> None:
+    with open("input_4.csv", "r") as fi:
+        with open("output_5.csv", "w") as fo:
+            for line in fi.readlines():
+                # skip \4 (type_)
+                out = pattern.sub(r"\1;\2;\3;\5;\6;\7;\8;\9;\10;\11", line)
+                fo.write(out)
+
+
+if __name__ == "__main__":
+    main()
